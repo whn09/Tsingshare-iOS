5e8d5f084021f4cdf17a6028e7d216d1dc26834d
whn09 <wanghenan09@gmail.com>
2015-02-13 20:25:20 +0800

    a tab view app using swift

---
+++ Tsingshare-iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tsingshare-iOS
+//
+//  Created by whn13 on 15/2/13.
+//  Copyright (c) 2015年 whn13. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
